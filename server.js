7acee8fc7f08e650957bce1f3294be462b8a0d3d
Amanda McClintock <amcclintock@mac-amcclintock1.cb.careerbuilder.com>
2017-11-02 17:31:18 -0400

    Modified server and added index

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/ping', function(req, res) {
-    res.send({ping: 'hello this is a server and I alive'});
-});
-
-app.get('/ping:id', function(req, res) {
-    res.send({ping: 'hello this is a server and I got ' +req.param.id});
-})
+app.use(express.static(__dirname));
 
 app.listen(3000);
-console.log('Listening on prot 3000...');
+console.log('Listening on port 3000...');
